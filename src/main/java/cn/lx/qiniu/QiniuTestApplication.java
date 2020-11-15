e2e3e4dcac9899533039fe32596c2e673d866b2c
lx972 <1160480264@qq.com>
2020-11-15 12:36:18 +0800

    第1次提交，使用spring脚手架初始化一个springboot项目

---
+++ src/main/java/cn/lx/qiniu/QiniuTestApplication.java
@@ -0,0 +1,16 @@
+package cn.lx.qiniu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ */
+@SpringBootApplication
+public class QiniuTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QiniuTestApplication.class, args);
+    }
+
+}
